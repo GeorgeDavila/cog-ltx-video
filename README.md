0aecd6fcddcabc68ab3004062dc45e49f330e03d
George Davila Durendal <gaddog94@gmail.com>
2025-01-09 14:55:34 -0500

    upload

--- README.md
+++ README.md
@@ -1,1 +1,62 @@
-# cog-ltx-video
+# SDXL-LoRA-beethoven-melspectrogram
+
+SDXL LoRAs can produce great image results. But can they produce coherent music by generating mel spectrograms(basically image versions of songs)? That's what we're experimenting with here.
+
+- Replicate API: https://replicate.com/georgedavila/sdxl-beethoven-spectrograms-lora
+- Model: https://huggingface.co/GDavila/sdxl-beethoven-spectrograms
+
+Trigger word: SPECTROGRAM
+
+- [X] Audio to image spectrogram scripts
+- [X] Model trained
+- [X] Model deployed to replicate
+- [ ] Image spectrogram to audio scripts
+
+## Make Spectrograms
+
+Place audio data in directory `audio_data` and run `python makeMels.py` to create spectrograms which will be placed in `mels` directory. 
+
+## Training
+Zip `mels` directory and use [sdxl-lora-customize-training](https://replicate.com/zylim0702/sdxl-lora-customize-training) for easy lora training. 
+
+
+### References 
+
+Putting an audio file in and out of these two source should generate a coherent output albeit with large loss of audio fidelity:
+- Generate spectrograms from audio online: https://convert.ing-now.com/audio-spectrogram-creator/
+- Convert Spectrogram image to audio online: https://alexadam.github.io/demos/img-encode/index.html (be sure to set the time to the time of the input audio)
+
+
+- Data sourced from https://www.chosic.com/free-music/beethoven/
+- https://github.com/OmarMedhat22/Sound-Classification-Mel-Spectrogram/blob/master/mel%20spectrogram.ipynb
+- https://importchris.medium.com/how-to-create-understand-mel-spectrograms-ff7634991056
+- https://stackoverflow.com/questions/60365904/reconstructing-audio-from-a-melspectrogram-has-some-clipping-with-librosa
+- https://librosa.org/doc/main/generated/librosa.feature.inverse.mel_to_audio.html#librosa.feature.inverse.mel_to_audio
+
+## Results
+
+[Examples](https://replicate.com/georgedavila/sdxl-beethoven-spectrograms-lora/examples)
+
+- 1) Prompt: "A SPECTROGRAM image", No Negative Prompt, width=1024, height=1024, guidanceScale=7.5, num_inference_steps=50
+![res1](results/res1.png)
+
+- 2) Prompt: "A SPECTROGRAM image", Negative Prompt: "fuzzy, lone pixels", width=1024, height=1024, guidanceScale=7.5, num_inference_steps=50
+![res2](results/res2.png)
+
+- 3) Prompt: "A SPECTROGRAM image", No Negative Prompt, width=640, height=480, guidanceScale=7.5, num_inference_steps=50
+![res3](results/res3.png)
+
+- 4) Prompt: "A SPECTROGRAM image", Negative Prompt: "noisy", width=640, height=480, guidanceScale=7.5, num_inference_steps=50
+![res4](results/res4.png)
+
+- 5) Prompt: "A SPECTROGRAM image", Negative Prompt: "noisy", width=1024, height=1024, guidanceScale=7.5, num_inference_steps=50
+![res5](results/res5.png)
+
+- 6) Prompt: "A photo of a dog in the style of SPECTROGRAM", Negative Prompt: "noisy", width=1024, height=1024, guidanceScale=7.5, num_inference_steps=50
+![res6](results/res6.png)
+
+- 7) Prompt: "A photo of a dog in the style of SPECTROGRAM", Negative Prompt: "noisy", width=1024, height=1024, guidanceScale=40.0, num_inference_steps=100
+![res7](results/res7.png)
+
+Clearly we can produce coherent spectorgrams using SDXL LoRAs, its not simply noise otherwise we wouldn't see clear waveforms and the black on top orange on bottom type structure. In [example 7](https://github.com/GeorgeDavila/SDXL-LoRA-beethoven-melspectrogram/blob/main/results/res7.png) we can see stratification layers ie striped bands emerge in the waveforms. Notably the swirls are a characteristic sign that stable diffusion can't fully denoise a region, so it's not necessarily fully denoising the wave forms. See [here](https://upload.wikimedia.org/wikipedia/commons/9/99/X-Y_plot_of_algorithmically-generated_AI_art_of_European-style_castle_in_Japan_demonstrating_DDIM_diffusion_steps.png) for an example of SD denoising - those swirls are artifacts of step 1. But overall these are very interesting results considering we only used 7 audio samples and only trained a LoRA. 
+
---
+++ predict.py
@@ -0,0 +1,124 @@
+# Prediction interface for Cog
+from cog import BasePredictor, Input, Path
+import os
+import sys
+import torch
+import shutil
+from PIL import Image
+from typing import List
+from diffusers import StableDiffusionXLPipeline
+
+base_model_path = "Lightricks/LTX-Video"
+#LoRA_PATH = "hfpath/sdxl-beethoven-spectrograms"
+#LoRA_file = "lora.safetensors"
+device = "cuda"
+MODEL_CACHE = "model-cache"
+
+import torch
+from diffusers import LTXPipeline
+from diffusers.utils import export_to_video
+
+def load_image(path): #for img2img
+        shutil.copyfile(path, "/tmp/image.png")
+        return load_image("/tmp/image.png").convert("RGB")
+
+class Predictor(BasePredictor):
+    def setup(self) -> None:
+        """Load the model into memory to make running multiple predictions efficient"""
+        # load SDXL pipeline
+        self.pipe = LTXPipeline.from_pretrained(
+            base_model_path, 
+            torch_dtype=torch.bfloat16,
+            use_safetensors=True,
+            watermark=None,
+            safety_checker=None,
+            #variant="fp16",
+        ).to(device)
+
+        #self.pipe.load_lora_weights(LoRA_PATH, weight_name=LoRA_file)
+
+    def predict(
+        self,
+        myprompt: str = Input(
+            description="Input prompt",
+            default="A woman with long brown hair and light skin smiles at another woman with long blonde hair. The woman with brown hair wears a black jacket and has a small, barely noticeable mole on her right cheek. The camera angle is a close-up, focused on the woman with brown hair's face. The lighting is warm and natural, likely from the setting sun, casting a soft glow on the scene. The scene appears to be real-life footage"
+        ),
+        #promptAddendum: str = Input(
+        #    description="Extra terms to add to end of prompt",
+        #    default="",
+        #),
+        negative_prompt: str = Input(
+            description="Negative Prompt",
+            default="worst quality, inconsistent motion, blurry, jittery, distorted"
+        ),
+        outWidth: int = Input(
+            description="width of output",
+            ge=128,
+            le=2048,
+            default=768, # 640
+        ),
+        outHeight: int = Input(
+            description="height of output",
+            ge=128,
+            le=2048,
+            default=512, # 480
+        ),
+        guidanceScale: float = Input(
+            description="Guidance scale (influence of input text on generation)",
+            ge=1.0,
+            le=10.0,
+            default=3.0
+        ),
+        num_frames: int = Input(
+            description="Number of images to output.",
+            default=97,
+            choices=[97, 129, 161, 193, 225, 257],
+        ),
+        num_outputs: int = Input(
+            description="Number of outputs.",
+            ge=1,
+            le=4,
+            default=1,
+        ),
+        num_inference_steps: int = Input(
+            description="Number of denoising steps", ge=1, le=500, default=50
+        ),
+        outFPS: int = Input(
+            description="Output FPS",
+            default=24,
+        ),
+        seed: int = Input(
+            description="Random seed. Leave blank to randomize the seed", default=None
+        ),
+    ) -> List[Path]:
+        """Run a single prediction on the model"""
+        while (myprompt[-1] == " ") or (myprompt[-1] == "\n"): #remove user whitespaces
+            myprompt = myprompt[:-1]
+        myprompt = myprompt #+ promptAddendum
+        if seed is None:
+            seed = int.from_bytes(os.urandom(2), "big")
+        print(f"Using seed: {seed}")
+
+        video = self.pipe(
+            prompt=myprompt,
+            negative_prompt=negative_prompt,
+            width=outWidth,
+            height=outHeight,
+            num_frames=num_frames,
+            decode_timestep=0.03,
+            decode_noise_scale=0.025,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidanceScale,
+            num_videos_per_prompt=num_outputs,
+        ).frames[0]
+
+        output_filename = "/tmp/output.mp4"
+        export_to_video(video, output_filename, fps=outFPS)
+        
+        #output_paths = []
+        #for i, _ in enumerate(imagesObj):
+        #    output_path = f"/tmp/out-{i}.png"
+        #    imagesObj[i].save(output_path)
+        #    output_paths.append(Path(output_path))
+            
+        return Path(output_filename)
